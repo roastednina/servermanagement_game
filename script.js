f81d35f473c716ac17484af6735b5adf01cdf027
edible_unicorn <109415752+roastednina@users.noreply.github.com>
2026-07-07 08:05:41 +1000

    ２

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
-// =========================
-// 初期設定
-// =========================
+// ===================================
+// 鯖主サバイバル
+// script.js
+// ===================================
 
+// 初期ステータス
 let mental = 15;
 let trust = 15;
+let serverType = "";
 
+// 質問数
 const maxQuestions = 7;
 
-// =========================
-// ランダム
-// =========================
+// ランダムに選ばれた質問
+let selectedQuestions = [];
+
+
+// ===================================
+// 初期化
+// ===================================
+
+init();
+
+
+// ===================================
+// 初期化
+// ===================================
+
+function init() {
+
+    loadQuestions();
+
+}
+
+
+// ===================================
+// 質問を読み込む
+// ===================================
+
+function loadQuestions() {
+
+    selectedQuestions = shuffle([...questions]).slice(0, maxQuestions);
+
+    showQuestions();
+
+}
+
+
+// ===================================
+// 質問表示
+// ===================================
+
+function showQuestions() {
+
+    console.log(selectedQuestions);
+
+}
+
+
+// ===================================
+// 配列をシャッフル
+// ===================================
 
 function shuffle(array) {
 
     return array.sort(() => Math.random() - 0.5);
 
 }
-
-const selectedQuestions = shuffle([...questions]).slice(0, maxQuestions);
-
-// =========================
-// 質問表示
-// =========================
-
-const questionArea = document.getElementById("questionArea");
-
-selectedQuestions.forEach((question, index) => {
-
-    questionArea.innerHTML += `
-
-    <div class="questionBox">
-
-        <p><strong>Q${index + 1}</strong><br>${question.text}</p>
-
-        <label>
-            <input type="radio"
-                   name="q${question.id}"
-                   value="yes">
-            はい
-        </label>
-
-        <label>
-            <input type="radio"
-                   name="q${question.id}"
-                   value="neutral">
-            どちらともいえない
-        </label>
-
-        <label>
-            <input type="radio"
-                   name="q${question.id}"
-                   value="no">
-            いいえ
-        </label>
-
-    </div>
-
-    <br>
-
-    `;
-
-});
